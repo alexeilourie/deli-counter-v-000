995a68a9c6ee918d49168fae8d28a3455fdd9087
Alexei Lourie <alexeilourie@gmail.com>
2018-10-25 16:24:53 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,8 @@ end
 def now_serving(katz_deli)
   if katz_deli.size == 0
     puts "There is nobody waiting to be served!"
+  else
+    name = katz_deli.shift
+    puts "Currently serving #{name}."
   end
-  name = katz_deli.shift
-  puts "Currently serving #{name}."
 end
