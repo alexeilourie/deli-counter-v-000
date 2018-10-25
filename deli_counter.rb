6090f5218681989b7bbd278a61ddcfea1853f7d8
Alexei Lourie <alexeilourie@gmail.com>
2018-10-25 15:58:25 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+def line(names)
+  line = "The line is currently:"
+  if names.size == 0
+    return "The line is currently empty."
+  else
+    names.each.with_index do |name, idx|
+      line << " #{idx+1}. #{name}"
+    end
+    return line
+  end
+end
