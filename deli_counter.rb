fa4d83391d431df2f9f6e2bb2d1a3fa4678669e7
Alexei Lourie <alexeilourie@gmail.com>
2018-10-25 16:29:37 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,7 +12,7 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli << name
-  idx = katz_deli(name)
+  idx = katz_deli.index(name)
   puts "Welcome, #{name}. You are number #{idx + 1} in line."
 end
 
